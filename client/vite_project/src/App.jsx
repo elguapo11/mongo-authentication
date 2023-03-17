d8986e40a4ce45f44d4427ed1f854589f55fab44
Benny Mendoza <bennybc9387@gmail.com>
2023-03-16 20:58:26 -0600

    started vite project/mongosh

---
+++ client/vite_project/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div>
+       
+      </div>
+      <h1>My testing site for Mongo</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          Every time you click you wish me success {count}
+        </button>
+        <p>
+          Me testing things out
+        </p>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
