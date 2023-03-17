a1f46aaf41cfd00ea66e913cf014743446b09db1
Benny Mendoza <bennybc9387@gmail.com>
2023-03-17 13:28:53 -0600

    added env/ignore files

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+require("dotenv").config();
+const { PORT } = process.env;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
