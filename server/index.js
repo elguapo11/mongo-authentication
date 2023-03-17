82bf5649b006eb0d796ce5be4132dfbf5a0ae8d7
Benny Mendoza <bennybc9387@gmail.com>
2023-03-17 14:07:13 -0600

    added basic routes

--- server/index.js
+++ server/index.js
@@ -1,12 +1,17 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 require("dotenv").config();
 const { PORT } = process.env;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/databases", (req, res) => {
+  console.log("testing when I hit the endpoint");
+  res.send('You have hit the endpoint')
+});
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
-})
+  console.log(`My site listening on port ${PORT}`);
+});
