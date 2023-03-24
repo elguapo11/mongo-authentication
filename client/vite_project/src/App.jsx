fe6c416fc3912706bb602da73b96ce1b74c4398c
Benny Mendoza <bennybc9387@gmail.com>
2023-03-24 14:50:42 -0600

    localhost is operations but with errors

--- client/vite_project/src/App.jsx
+++ client/vite_project/src/App.jsx
@@ -4,6 +4,8 @@ import { useState } from 'react'
 
 import './App.css'
 import './main.html'
+import { createHtmlPlugin } from 'vite-plugin-html'
+
 
 function App() {
   const [count, setCount] = useState(0)
