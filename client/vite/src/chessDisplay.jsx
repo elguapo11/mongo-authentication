56a99dc91fcb9b76b9a4430e0105a8d8738498b0
Benny Mendoza <bennybc9387@gmail.com>
2023-04-16 15:29:49 -0600

    added notes for clarification

---
+++ client/vite/src/chessDisplay.jsx
@@ -0,0 +1,10 @@
+function chessDisplay () {
+    console.log('hello world ')
+}
+
+chessDisplay()
+
+//This component will be the main chess display where the user will actually rendered chess moves
+//this is the app that should also be where the main action takes place
+
+export default chessDisplay
