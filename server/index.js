0669965b5008fa14533bc441cdee82e281b8349a
Benny Mendoza <bennybc9387@gmail.com>
2023-10-06 15:26:15 -0600

    publishing changes

--- server/index.js
+++ server/index.js
@@ -1,22 +1,25 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const app = express();
 const { PORT } = process.env;
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const test = require('./dbroutes');
 
+mongoose.connect(process.env.DATABASE_URL, { useNewURLParser: true });
+const db = mongoose.connection;
 
-
-mongoose.connect(process.env.DATABASE_URL, {useNewURLParser:true})
-const db = mongoose.connection
-
-db.on('error', (error) => console.error(error))
-db.on('open', () => console.log('connected to database'))
+db.on('error', (error) => console.error(error));
+db.on('open', () => console.log('connected to database'));
 
 app.get('/', (req, res) => {
-  res.send('WELCOME TO MY SERVER!')
-})
+  res.send(test);
+});
+
+app.get('/api', (req, res) => {
+  res.send('You have hit the api page');
+  console.log('You have hit the api page');
+});
 
 app.listen(PORT, () => {
-  console.log(`My server is running on ${PORT}`)
-})
-
+  console.log(`My server is running on ${PORT}`);
+});
