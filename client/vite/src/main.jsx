ada5c7eaf5cd074bdfd89da38d06f53746b0dab4
Benny Mendoza <bennybc9387@gmail.com>
2023-10-27 22:25:15 -0600

    updated and tried to fix a bunch of crap

--- client/vite/src/main.jsx
+++ client/vite/src/main.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 import './index.css'
-import Navbar from './navbar'
-import Piece from './chessDisplay'
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Navbar/>
+    <App />
   </React.StrictMode>,
 )
