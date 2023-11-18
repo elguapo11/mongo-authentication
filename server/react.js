f5dfb9fda28955b126b9b543deca0547211c9ce9
Benny Mendoza <bennybc9387@gmail.com>
2023-11-17 20:01:28 -0700

    messing around with functions

--- server/react.js
+++ server/react.js
@@ -2,10 +2,10 @@ export function display() {
   return ' Sample function to show client';
 }
 
-export function show() {
+export function show(x) {
   //Notice the 'export' which allows me to export to client
 
-  return ' displaying how to output multiple functions';
+  return x + ' displaying how to output multiple functions';
 }
 
 //This is simply a test to see how to import simple functions to the client and seeing the folder structure and exports.
