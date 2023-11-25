8bb1f119f09fdac95061a7493005789daa8be035
Benny Mendoza <bennybc9387@gmail.com>
2023-11-25 11:52:25 -0700

    created posts.jsx for readability

--- client/vite/src/App.jsx
+++ client/vite/src/App.jsx
@@ -3,28 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { show , display} from '../../../server/react'
-import axios from 'axios'; // Import axios for making HTTP requests
 
 
 
 function App() {
   const [count, setCount] = useState(0)
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    // Fetch all posts when the component mounts
-    const fetchPosts = async () => {
-      try {
-        const response = await axios.get('http://localhost:3000/posts/get');
-        setPosts(response.data);
-      } catch (error) {
-        console.error('Error fetching posts:', error);
-      }
-    };
-
-    fetchPosts();
-  }, []);
-
 
   return (
     <>
@@ -39,13 +22,6 @@ function App() {
       <h1>{display()}</h1>
       <h1>{show('hello')}</h1>
       <h1>Vite + React</h1>
-      <div className="posts">
-        {posts.map((post) => (
-          <div key={post._id}>
-            <h2>{post.Title}</h2>
-          </div>
-        ))}
-      </div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
