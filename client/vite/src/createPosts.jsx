0c9799e2873ddd374fac850f15667312c8952d9b
Benny Mendoza <bennybc9387@gmail.com>
2023-12-01 13:36:23 -0700

    added tag

--- client/vite/src/createPosts.jsx
+++ client/vite/src/createPosts.jsx
@@ -4,12 +4,14 @@ import { useRef } from 'react';
 function Create() {
     const titleRef = useRef();
     const contentRef = useRef();
+    const tagRef = useRef();
   
     function onSubmit(e) {
       e.preventDefault();
       const postBody = {
         Title: titleRef.current.value,
         Content: contentRef.current.value,
+        Tag: tagRef.current.value,
       };
   
       axios
@@ -23,8 +25,11 @@ function Create() {
         <label htmlFor="title">Title</label>
         <input ref={titleRef} type="title" id="title" />
         <span></span>
-        <label htmlFor="title">Content</label>
-        <input ref={contentRef} type="entry" id="entry" />
+        <label htmlFor="content">Content</label>
+        <input ref={contentRef} type="content" id="content" />
+        <span></span>
+        <label htmlFor="tag">tag</label>
+        <input ref={tagRef} type="tag" id="tag" />
         <button type="submit">Submit</button>
         
       </form>
