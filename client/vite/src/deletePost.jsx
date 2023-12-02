cca976cfb4c0f108a609b429329c6d9a7b0ff482
Benny Mendoza <bennybc9387@gmail.com>
2023-12-01 18:54:58 -0700

    saving Delete function

---
+++ client/vite/src/deletePost.jsx
@@ -0,0 +1,26 @@
+import { useRef } from "react";
+import axios from "axios";
+
+
+
+function Delete() {
+    const postIdRef = useRef();
+
+  function onSubmit(e) {
+    e.preventDefault();
+    const postId = postIdRef.current.value; 
+
+    axios
+      .delete(`http://localhost:3000/posts/delete/${postId}`)
+      .then((response) => console.log(`Post Id ${postId} has been deleted`));
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <label htmlFor="Delete Post"> Delete Post</label>
+      <button type="submit">Submit</button>
+      <input ref={postIdRef} type="ID" id="ID" />
+    </form>
+  );
+}
+export default Delete;
