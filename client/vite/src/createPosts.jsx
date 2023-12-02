aeff3c0cabc6778402ec09336e39a66b5d45c5ea
Benny Mendoza <bennybc9387@gmail.com>
2023-12-01 19:22:35 -0700

    reload saved

--- client/vite/src/createPosts.jsx
+++ client/vite/src/createPosts.jsx
@@ -2,38 +2,38 @@ import axios from 'axios';
 import { useRef } from 'react';
 
 function Create() {
-    const titleRef = useRef();
-    const contentRef = useRef();
-    const tagRef = useRef();
-  
-    function onSubmit(e) {
-      e.preventDefault();
-      const postBody = {
-        Title: titleRef.current.value,
-        Content: contentRef.current.value,
-        Tag: tagRef.current.value,
-      };
-  
-      axios
-        .post("http://localhost:3000/posts/create", postBody)
-  
-        .then((response) => console.log('post created'));
-    }
-  
-    return (
-      <form onSubmit={onSubmit}>
-        <label htmlFor="title">Title</label>
-        <input ref={titleRef} type="title" id="title" />
-        <span></span>
-        <label htmlFor="content">Content</label>
-        <input ref={contentRef} type="content" id="content" />
-        <span></span>
-        <label htmlFor="tag">tag</label>
-        <input ref={tagRef} type="tag" id="tag" />
-        <button type="submit">Submit</button>
-        
-      </form>
-    );
+  const titleRef = useRef();
+  const contentRef = useRef();
+  const tagRef = useRef();
+
+  function onSubmit(e) {
+    e.preventDefault();
+    const postBody = {
+      Title: titleRef.current.value,
+      Content: contentRef.current.value,
+      Tag: tagRef.current.value,
+    };
+
+    axios
+      .post('http://localhost:3000/posts/create', postBody)
+
+      .then((response) => console.log('post created'));
+    window.location.reload();
   }
-  
-  export default Create;
+
+  return (
+    <form onSubmit={onSubmit}>
+      <label htmlFor='title'>Title</label>
+      <input ref={titleRef} type='title' id='title' />
+      <span></span>
+      <label htmlFor='content'>Content</label>
+      <input ref={contentRef} type='content' id='content' />
+      <span></span>
+      <label htmlFor='tag'>tag</label>
+      <input ref={tagRef} type='tag' id='tag' />
+      <button type='submit'>Submit</button>
+    </form>
+  );
+}
+
+export default Create;
