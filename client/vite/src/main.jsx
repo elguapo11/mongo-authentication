4b5dbcf1c9faf35d159d72262b4a8895a39301e5
Benny Mendoza <bennybc9387@gmail.com>
2023-12-01 19:24:25 -0700

    saving formatting changes

--- client/vite/src/main.jsx
+++ client/vite/src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import Posts from './getPosts.jsx'
-import Create from './createPosts.jsx'
-import Delete from './deletePost.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import Posts from './getPosts.jsx';
+import Create from './createPosts.jsx';
+import Delete from './deletePost.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -12,7 +12,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Posts />
     <Create />
     <Delete />
-    
-  </React.StrictMode>,
-)
-
+  </React.StrictMode>
+);
