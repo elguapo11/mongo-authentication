908510c358fe17a7236b0d39036568bfa8bf51ee
Benny Mendoza <bennybc9387@gmail.com>
2023-12-07 21:37:05 -0700

    edit component added

---
+++ client/vite/src/editPost.jsx
@@ -0,0 +1,45 @@
+import { useState, useRef } from 'react';
+import axios from 'axios';
+
+function EditPost({ postId, handleEdit }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const updatedContentRef = useRef();
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleCancelClick = () => {
+    setIsEditing(false);
+  };
+
+  const handleSaveClick = async () => {
+    try {
+      const updatedContent = updatedContentRef.current.value;
+
+      // Call the handleEdit function passed from the parent component
+      await handleEdit(postId, updatedContent);
+
+      // Reset the editing state
+      setIsEditing(false);
+    } catch (error) {
+      console.error('Error updating post:', error);
+    }
+  };
+
+  return (
+    <div>
+      {!isEditing ? (
+        <button onClick={handleEditClick}>Edit</button>
+      ) : (
+        <div>
+          <textarea ref={updatedContentRef} defaultValue={'test'} />
+          <button onClick={handleSaveClick}>Save</button>
+          <button onClick={handleCancelClick}>Cancel</button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default EditPost;
