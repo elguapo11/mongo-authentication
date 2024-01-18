2f8e7dc98ffab6d537c8ea09a24977d2600833ff
Benny Mendoza <bennybc9387@gmail.com>
2024-01-17 21:45:46 -0700

    fixed everything, don't know why I can't link to another page

--- client/vite/src/App.jsx
+++ client/vite/src/App.jsx
@@ -2,21 +2,17 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import Page from './Page';
+import Page from './Pages/Page';
 import { Link, Routes, BrowserRouter as Router } from 'react-router-dom';
 
 import { show, display } from '../../../server/react';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <Router>
-      <h2>
-        <Link to='/Page'>Hello World</Link>
-      </h2>
-      <Routes path='/Page' element={<Page />}></Routes>
-    </Router>
+    <>
+      <h2>Hello World</h2>
+      <Page />
+    </>
   );
 }
 
--- client/vite/src/Page.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-function Page() {
-  return <h2>Hello World</h2>;
-}
-export default Page;
